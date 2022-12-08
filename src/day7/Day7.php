326b184edcdefeffd5e4cb08676613a7503c09e1
Andreas Halemba ext <a.halemba.ext@careerpartner.eu>
2022-12-08 09:16:36 +0100

    ✨feature: day7: recusion sucks :D

---
+++ src/day7/Day7.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace App\day7;
+
+use Exception;
+
+class Day7
+{
+    private array $file;
+
+
+    public function __construct(?string $test)
+    {
+        if ($test) {
+            $this->file = file('./src/day7/test.txt', FILE_IGNORE_NEW_LINES);
+        } else {
+            $this->file = file('./src/day7/input.txt', FILE_IGNORE_NEW_LINES);
+        }
+        $this->parseFile();
+    }
+
+    public function parseFile()
+    {
+        $lines = [];
+        $lines = array_filter($this->file, function ($line) {
+            return preg_match('/^\$\s(cd)/', $line) || preg_match('/^(\d*)\s(\w*)/', $line);
+        });
+        [$tree, $_] = $this->processLines(array_values($lines), []);
+        [$totalSize, $folderSizeList] = $this->calcDirSizes($tree, []);
+        $relevantFoldersList = array_filter($folderSizeList, fn ($size) => $size <= 100000);
+        var_dump(array_sum($relevantFoldersList));
+        $requiredSpace = 30000000 - (70000000 - $totalSize);
+        $deletableFolders = array_filter($folderSizeList, fn ($folderSize) => $folderSize > $requiredSpace);
+        asort($deletableFolders);
+        var_dump($deletableFolders);
+    }
+
+    public function processLines(array $lines, array $tree): array
+    {
+        for ($lineNumber = 0; $lineNumber < count($lines); $lineNumber++) {
+            if (preg_match('/^\$\s(cd)\s(\.\.)$/', $lines[$lineNumber], $command)) {
+                return [$tree, $lineNumber];
+            } elseif (preg_match('/^\$\s(cd)\s(\S+)$/', $lines[$lineNumber], $command)) {
+                $linesLeft = array_slice($lines, $lineNumber + 1);
+                [$branch, $line] = $this->processLines($linesLeft, []);
+                $lineNumber += $line + 1;
+                $tree[$command[2]] = $branch;
+            } elseif (preg_match('/^(\d*)\s(\S*)/', $lines[$lineNumber], $file)) {
+                $tree[$file[2]] = $file[1];
+            } else {
+                throw new Exception('hin');
+            }
+        }
+        return [$tree, count($lines)];
+    }
+
+    public function calcDirSizes(array $tree)
+    {
+        $contentSizeSum = 0;
+        $subFolderSizes = [];
+        foreach ($tree as $folderContent) {
+            if (gettype($folderContent) === 'array') {
+                [$folderSize, $folderSubSize] = $this->calcDirSizes($folderContent);
+                $contentSizeSum += $folderSize;
+                $subFolderSizes = array_merge($subFolderSizes, $folderSubSize);
+            } else {
+                $contentSizeSum += $folderContent;
+            }
+        }
+        $subFolderSizes[] = $contentSizeSum;
+        return [$contentSizeSum, $subFolderSizes];
+    }
+}
--- src/index.php
+++ src/index.php
@@ -7,6 +7,7 @@ use App\day3\Day3;
 use App\day4\Day4;
 use App\day5\Day5;
 use App\day6\Day6;
+use App\day7\Day7;
 
 match ($argv[1]) {
     'day2' => new Day2(),
@@ -14,5 +15,6 @@ match ($argv[1]) {
     'day4' => new Day4(),
     'day5' => new Day5(),
     'day6' => new Day6($argv[2]),
+    'day7' => new Day7($argv[2] ?? null),
     default => print_r("wrong day given")
 };
